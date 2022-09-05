a87dc1ff1d323c65859e51d462f491b8820cdd24
jorgengj <jorgen.jarva@hotmail.com>
2022-09-05 23:36:20 +0300

    Lõin uue rakenduse

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,10 @@
+﻿//Nii sisestatakse kommentaar käesolevale koodile.
+
+//Ülesanne küsida kasutaja nime
+//Sellest järgnevalt tervitada kasutajat nimepidi
+
+Console.WriteLine("Sisesta oma nimi:");
+
+string Username = Console.ReadLine();
+
+Console.WriteLine($"Tere, {Username}!");
